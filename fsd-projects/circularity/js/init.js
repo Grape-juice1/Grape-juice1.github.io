1ad5d8aec1fb22b6bdc15c9e46cd93638ed383ef
Jelly Beans <judeducote920@gmail.com>
2026-04-23 14:05:38 +0000

    message

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -14,17 +14,24 @@ var init = function (window) {
         
         window.opspark.game = {};
         var game = window.opspark.game;
-        
+    
+
         ///////////////////
         // PROGRAM SETUP //
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+var circle;
+var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+       function drawCircle(){
+  circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas, 5, 5);
+view.addChild(circle);
+circles.push(circle);
+        }
 
 
         // TODO 3 : Call the drawCircle() function
@@ -33,7 +40,9 @@ var init = function (window) {
 
         // TODO 7 : Use a loop to create multiple circles
 
-
+for (var i = 0; i < 500; i++) {
+ drawCircle()
+}
 
 
         ///////////////////
@@ -53,9 +62,13 @@ var init = function (window) {
            
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+           for(var i = 0; i < circles.length; i++) {
+physikz.updatePosition(circles[i]);
+game.checkCirclePosition(circles[i]);
+           } 
+
         }
+        
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -63,18 +76,26 @@ var init = function (window) {
         it to the opposite side of the screen.
         */
         game.checkCirclePosition = function(circle) {
-
+        
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if(circle.x < 0) {
+    circle.x = canvas.width;
+}
+if(circle.y > canvas.height) {
+    circle.y = 0;
+}
+if(circle.y > canvas.height) {
+    circle.y = 0
+}
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        }
+       }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -94,7 +115,7 @@ var init = function (window) {
 
 // DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
-    (typeof process.versions.node !== 'undefined')) {
+    (typeof process.versions.node !== 'undefined')){
     // here, export any references you need for tests //
     module.exports = init;
 }
